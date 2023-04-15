9ee8cc3ca031a24bf5fed9f89ee520de784a2306
Jackson Sanders <43209556+jacksonjsanders@users.noreply.github.com>
2023-04-14 19:28:05 -0500

    round 2

---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_060914) do
+  create_table "balances", force: :cascade do |t|
+    t.float "value", default: 0.0
+    t.integer "wallet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "currency"
+    t.index ["wallet_id"], name: "index_balances_on_wallet_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "issuer"
+    t.string "holder_name"
+    t.string "number"
+    t.string "expiration_date"
+    t.string "cvv"
+    t.integer "wallet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wallet_id"], name: "index_cards_on_wallet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id", unique: true
+  end
+
+  add_foreign_key "balances", "wallets"
+  add_foreign_key "cards", "wallets"
+  add_foreign_key "wallets", "users"
+end
